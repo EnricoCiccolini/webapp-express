25c3472e04b3c9c8f601e0e9ddde2e63c13dd3ef
EnricoCiccolini <piroroma90@gmail.com>
2025-05-09 14:59:36 +0200

    feat: add middleware not found and error

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3004
 const filmsRouter = require('./router/films')
-
+const notFound = require('./middleware/notFound')
+const errorHandler = require('./middleware/errorhandler')
 
 
 app.use(express.json())
 
 
 app.use('/films', filmsRouter)
+
+
+app.use(errorHandler)
+app.use(notFound)
+
 
 app.listen(port, () => {
     console.log(`sono in ascolto sulla porta ${port}`)
