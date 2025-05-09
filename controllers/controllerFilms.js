35ea8272f548d2e86cada7344522b64833807b43
EnricoCiccolini <piroroma90@gmail.com>
2025-05-09 14:35:36 +0200

    feat add index

--- controllers/controllerFilms.js
+++ controllers/controllerFilms.js
@@ -1,11 +1,22 @@
 const connection = require('../data/db')
-function index(req, res) {
 
-    res.json("sono nell index")
+
+function index(req, res) {
+    const sql = 'SELECT * FROM `db-films`.movies'
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
+        res.json(results)
+    })
+
 }
 
 
 function show(req, res) {
+
+
+
+
+
 
     res.json("sono nello show")
 }
