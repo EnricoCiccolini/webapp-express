a5198f316dde2d2375950d9399831095bcb07a5c
EnricoCiccolini <piroroma90@gmail.com>
2025-05-09 14:47:22 +0200

    feat : add show whit rewiews

--- controllers/controllerFilms.js
+++ controllers/controllerFilms.js
@@ -13,12 +13,29 @@ function index(req, res) {
 
 function show(req, res) {
 
+    const { id } = req.params;
+
+    const filmsSql = 'SELECT * FROM `db-films`.movies WHERE id = ?';
+
+    const rewiuwsSql = `
+    SELECT *
+    FROM reviews
+    JOIN movies ON movies.id = reviews.movie_id
+    WHERE movies.id = 1
+    `;
 
 
 
-
-
-    res.json("sono nello show")
+    connection.query(filmsSql, [id], (err, filmsResults) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (filmsResults.length === 0) return res.status(404).json({ error: 'movie  not found' });
+        const film = filmsResults[0];
+        connection.query(rewiuwsSql, [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+            film.tags = reviewsResults;
+            res.json(film);
+        });
+    });
 }
 
 
