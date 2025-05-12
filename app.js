b4e1e6333750d17b0a2c6fa073cca8f508c7c1cb
EnricoCiccolini <piroroma90@gmail.com>
2025-05-12 14:01:35 +0200

    feat : install cors

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ const filmsRouter = require('./router/films')
 const notFound = require('./middleware/notFound')
 const errorHandler = require('./middleware/errorhandler')
 
+app.use(cors({
+    origin: process.env.FE_APP
+}))
+
 
 app.use(express.json())
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.send('benvenuto nel databese')
+})
 
 
 app.use('/films', filmsRouter)
 
-app.use(express.static('public'))
+
+
+
 app.use(errorHandler)
 app.use(notFound)
 
